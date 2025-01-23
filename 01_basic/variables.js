7bbf1e999c0cbad20e159bfd8ecb74d92955dd27
PalDadhaniya <163087900+PalDadhaniya@users.noreply.github.com>
2025-01-23 06:18:56 +0000

    pal

---
+++ 01_basic/variables.js
@@ -0,0 +1,9 @@
+const accountId = 1323
+let email = "palpatel3656@gmail.com"
+var password ="pal123"
+accountcity= "rajkot"
+
+// accountId=2
+
+console.log(accountId);
+console.table([accountId,email,password,accountcity])
